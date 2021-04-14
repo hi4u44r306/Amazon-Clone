21c7893f4d0e50bca40bf769e92b91accf93e0f2
victor <hi4u44r306@gmail.com>
2021-04-14 10:47:35 +0800

    Payment section (still can't deploy)

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,7 +6,7 @@ import Subtotal from "./Subtotal";
 
 function Checkout() {
 
-     const [{ basket }, dispatch] = useStateValue();
+     const [{ basket, user }, dispatch] = useStateValue();
 
      return (
           <div className='checkout'>
@@ -18,6 +18,7 @@ function Checkout() {
                     />
 
                     <div>
+                         <h3>Hello, {user?.email}</h3>
                          <h2 className="checkout__title">
                               Your shopping Basket
                          </h2>
